96ecb2c2eef8ced2a1cd99dc6d7fe3e23b76c097
Federico Cecchetto <fedecek3@gmail.com>
2020-09-25 18:12:29 +0200

    Use localized strings

--- src/client/module/discord_rpc.cpp
+++ src/client/module/discord_rpc.cpp
@@ -7,6 +7,7 @@
 
 #include "discord_register.h"
 #include "discord_rpc.h"
+#include "localized_strings.hpp"
 
 #include "utils/string.hpp"
 #include <string.h>
@@ -15,7 +16,6 @@ namespace
 {
 	DiscordEventHandlers handlers;
 	DiscordRichPresence discordPresence;
-	const char* localization[] = { "Multiplayer", "Singleplayer", "Main menu", "%s on %s", "Extinction", "Private match" };
 	auto ingame = false;
 
 	int get_player_count()
@@ -32,12 +32,11 @@ namespace
 
 	void tick()
 	{
-
 		discordPresence.largeImageKey = "iw6x";
 
 		if (!game::is_mp())
 		{
-			discordPresence.details = localization[1];
+			discordPresence.details = game::native::SEH_StringEd_GetString("DISCORDRPC_SP_TITLE");
 			Discord_UpdatePresence(&discordPresence);
 			return;
 		}
@@ -45,18 +44,17 @@ namespace
 		discordPresence.partySize = 0;
 		discordPresence.partyMax = 0;
 
-		discordPresence.details = game::native::Dvar_FindVar("g_gametype") ? strstr(game::native::Dvar_FindVar("g_gametype")->current.string, "aliens") ? localization[4] : localization[0] : "";
-		discordPresence.state = localization[2];
+		discordPresence.details = game::native::Dvar_FindVar("g_gametype") ? strstr(game::native::Dvar_FindVar("g_gametype")->current.string, "aliens") ? game::native::SEH_StringEd_GetString("DISCORDRPC_ZM_TITLE") : game::native::SEH_StringEd_GetString("DISCORDRPC_MP_TITLE") : "";
+		discordPresence.state = game::native::SEH_StringEd_GetString("DISCORDRPC_MAIN_MENU");
 
 		if (game::native::SV_Loaded())
 		{
 			discordPresence.partySize = get_player_count();
 			discordPresence.partyMax = game::native::Dvar_FindVar("sv_maxclients")->current.integer;
 
-			discordPresence.state = strstr(game::native::Dvar_FindVar("net_ip")->current.string, "localhost") ? localization[5] : game::native::Dvar_FindVar("sv_hostname")->current.string;
-			discordPresence.details = utils::string::va(localization[3], game::native::Dvar_FindVar("party_gametype")->current.string, game::native::Dvar_FindVar("party_mapname")->current.string);
+			discordPresence.state = strstr(game::native::Dvar_FindVar("net_ip")->current.string, "localhost") ? game::native::SEH_StringEd_GetString("DISCORDRPC_PRIVATE_MATCH") : game::native::Dvar_FindVar("sv_hostname")->current.string;
+			discordPresence.details = utils::string::va(game::native::SEH_StringEd_GetString("DISCORDRPC_MATCH_TEMPLATE"), game::native::Dvar_FindVar("party_gametype")->current.string, game::native::Dvar_FindVar("party_mapname")->current.string);
 		}
-
 
 		if (!ingame)
 			discordPresence.startTimestamp = game::native::SV_Loaded() ? std::time(0) : NULL;
@@ -72,6 +70,13 @@ class discord_rpc final : public module
 public:
 	void post_unpack() override
 	{
+		localized_strings::override("DISCORDRPC_MP_TITLE", "Multiplayer");
+		localized_strings::override("DISCORDRPC_SP_TITLE", "Singleplayer");
+		localized_strings::override("DISCORDRPC_ZM_TITLE", "Extinction");
+		localized_strings::override("DISCORDRPC_MATCH_TEMPLATE", "%s on %s");
+		localized_strings::override("DISCORDRPC_PRIVATE_MATCH", "Private Match");
+		localized_strings::override("DISCORDRPC_MAIN_MENU", "Main menu");
+
 		memset(&handlers, 0, sizeof(handlers));
 		Discord_Initialize("757965986954477690", &handlers, 1, "");
 		discordPresence.startTimestamp = std::time(0);
